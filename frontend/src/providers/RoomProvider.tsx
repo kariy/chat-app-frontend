d0476709bf3f53c3d299ba94ffa86c6d946a4ca4
Ammar Arif <ammr.arf@gmail.com>
2021-10-09 12:46:48 +0800

    now call approriate socket emits upon set/remove

--- frontend/src/providers/RoomProvider.tsx
+++ frontend/src/providers/RoomProvider.tsx
@@ -1,47 +1,35 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { addToArrayWithEffect, deleteInArrayWithEffect } from "../utils/array";
+import { ChatSocketContext } from "./ChatSocketProvider";
 
-interface TRoomContext {
+type TRoomContext = {
     list: string[];
     set: (room: string | string[]) => void;
     remove: (room: string | string[]) => void;
-}
+};
 
 export const RoomContext = createContext<TRoomContext | null>(null);
 
 const RoomProvider: React.FC = ({ children }) => {
+    const socket = useContext(ChatSocketContext);
     const [roomList, setRoomList] = useState<string[]>([]);
 
-    // useEffect(() => {
-    //     console.log("list of connected rooms : ", roomList);
-    // }, [roomList]);
-
-    function set(room: string | string[]) {
-        const arr = [...roomList];
-
-        if (typeof room === "string") arr.push(room);
-        else if (typeof room === "object")
-            room.forEach((room) => arr.push(room));
-
-        setRoomList(arr);
+    function add(room: any | any[]) {
+        const list = addToArrayWithEffect(roomList, room, (room) => {
+            socket?.join(room);
+        });
+        setRoomList(list);
     }
 
-    function remove(room: string | string[]) {
-        const arr = [...roomList];
-
-        if (typeof room === "string" && isRoomExist(room))
-            arr.splice(arr.indexOf(room), 1);
-        else if (typeof room === "object")
-            room.forEach((item) => {
-                if (isRoomExist(item)) arr.splice(arr.indexOf(item), 1);
-            });
-    }
-
-    function isRoomExist(room: string) {
-        return roomList.indexOf(room) > -1 ? true : false;
+    function remove(room: any | any[]) {
+        const list = deleteInArrayWithEffect(roomList, room, (room) => {
+            socket?.leave(room);
+        });
+        setRoomList(list);
     }
 
     return (
-        <RoomContext.Provider value={{ list: roomList, set, remove }}>
+        <RoomContext.Provider value={{ list: roomList, set: add, remove }}>
             {children}
         </RoomContext.Provider>
     );
