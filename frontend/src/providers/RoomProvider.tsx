a26685de8d1b9a8bf7e38c3d46d6a57153c51849
Ammar Arif <ammr.arf@gmail.com>
2021-10-11 22:48:05 +0800

    added utility methods

--- frontend/src/providers/RoomProvider.tsx
+++ frontend/src/providers/RoomProvider.tsx
@@ -1,35 +1,142 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { addToArrayWithEffect, deleteInArrayWithEffect } from "../utils/array";
-import { ChatSocketContext } from "./ChatSocketProvider";
+import AVAILABLE_ROOMS, { TRoom } from "../stores/rooms";
+import {
+    addToArrayWithEffect,
+    deleteInArrayWithEffect,
+    isArray,
+} from "../utils/array";
+import { ChatSocketMethodContext } from "./ChatSocketProvider";
+import { ConversationContext } from "./ConversationContext";
 
 type TRoomContext = {
-    list: string[];
-    set: (room: string | string[]) => void;
-    remove: (room: string | string[]) => void;
+    list: TRoom[];
+    totalRoom: number;
+    init: (ids: TRoom["id"][]) => void;
+    add: (id: TRoom["id"]) => TRoom | undefined;
+    addMany: (ids: TRoom["id"][]) => TRoom[];
+    remove: (id: TRoom["id"]) => void;
+    removeMany: (ids: TRoom["id"][]) => void;
+    get: (id: TRoom["id"]) => TRoom | undefined;
 };
 
 export const RoomContext = createContext<TRoomContext | null>(null);
 
 const RoomProvider: React.FC = ({ children }) => {
-    const socket = useContext(ChatSocketContext);
-    const [roomList, setRoomList] = useState<string[]>([]);
+    // Socket
+    const convoCtx = useContext(ConversationContext);
+    const chatSocketMethods = useContext(ChatSocketMethodContext);
 
-    function add(room: any | any[]) {
-        const list = addToArrayWithEffect(roomList, room, (room) => {
-            socket?.join(room);
-        });
-        setRoomList(list);
+    // State
+    const [roomList, setRoomList] = useState<TRoom[]>([]);
+
+    useEffect(() => {
+        console.log("room list ", roomList);
+    }, [roomList]);
+
+    function init(ids: TRoom["id"][]) {
+        const roomsAdded = addMany(ids);
+
+        if (roomsAdded.length) convoCtx?.init(roomsAdded);
     }
 
-    function remove(room: any | any[]) {
-        const list = deleteInArrayWithEffect(roomList, room, (room) => {
-            socket?.leave(room);
+    function fetchRoomInfo(id: string): TRoom | undefined {
+        return AVAILABLE_ROOMS.find((item) => item.id === id);
+    }
+
+    function add(id: TRoom["id"]) {
+        if (!includes(id)) return;
+
+        const roomsToAdd = fetchRoomInfo(id);
+
+        if (!roomsToAdd) return;
+
+        const newList = [...roomList];
+        newList.push(roomsToAdd);
+
+        chatSocketMethods?.join(id);
+        setRoomList(newList);
+
+        return roomsToAdd;
+    }
+
+    function addMany(ids: TRoom["id"][]) {
+        let roomsToAdd: TRoom[] = [];
+
+        ids.forEach((id) => {
+            if (includes(id)) return;
+
+            const roomInfo = fetchRoomInfo(id);
+
+            if (!roomInfo) return;
+
+            roomsToAdd.push(roomInfo);
+            chatSocketMethods?.join(id);
         });
-        setRoomList(list);
+
+        const newList = roomList.concat(roomsToAdd);
+        setRoomList(newList);
+
+        return roomsToAdd;
+    }
+
+    function remove(id: TRoom["id"]) {
+        setRoomList((prev) => {
+            const idxToDel = findIndexOf(id);
+
+            if (idxToDel === -1) return prev;
+
+            const newList = [...prev];
+            newList.splice(idxToDel, 1);
+            chatSocketMethods?.leave(id);
+            return newList;
+        });
+    }
+
+    function removeMany(ids: TRoom["id"][]) {
+        setRoomList((prev) => {
+            const newList = [...prev];
+
+            ids.forEach((id) => {
+                const idxToDel = findIndexOf(id);
+
+                if (idxToDel === -1) return;
+
+                newList.splice(idxToDel, 1);
+                chatSocketMethods?.leave(id);
+            });
+
+            return newList;
+        });
+    }
+
+    function findIndexOf(id: TRoom["id"]) {
+        return roomList.findIndex((room) => room.id === id);
+    }
+
+    function includes(id: TRoom["id"]) {
+        const idx = findIndexOf(id);
+        console.log(idx);
+
+        return idx !== -1 ? true : false;
+    }
+
+    function get(id: TRoom["id"]) {
+        return roomList.find((room) => room.id === id);
     }
 
     return (
-        <RoomContext.Provider value={{ list: roomList, set: add, remove }}>
+        <RoomContext.Provider
+            value={{
+                init,
+                list: roomList,
+                totalRoom: roomList.length,
+                add,
+                remove,
+                addMany,
+                removeMany,
+                get,
+            }}
+        >
             {children}
         </RoomContext.Provider>
     );
